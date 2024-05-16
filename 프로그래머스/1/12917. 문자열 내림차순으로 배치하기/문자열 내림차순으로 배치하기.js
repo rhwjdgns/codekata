190807ccc894004c2096017860aa3fa739ee462a
rhwjdgns <167050760+rhwjdgns@users.noreply.github.com>
2024-05-17 08:44:46 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -0,0 +1,5 @@
+function solution(s) {
+    var upperCase = s.split('').filter(char => char.toUpperCase() === char).sort().reverse().join('');
+    var lowerCase = s.split('').filter(char => char.toLowerCase() === char).sort().reverse().join('');
+    return lowerCase + upperCase;
+}
