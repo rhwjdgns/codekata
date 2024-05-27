8e00c483ae61e440ce40435978b112dd16cb3ed3
rhwjdgns <167050760+rhwjdgns@users.noreply.github.com>
2024-05-28 08:53:43 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,9 @@
+function solution(t, p) {
+    var answer = 0;
+    for (var i = 0; i <= t.length - p.length; i++) {
+        if (parseInt(t.substring(i, i + p.length)) <= parseInt(p)) {
+            answer++;
+            }
+    }
+    return answer;
+}
