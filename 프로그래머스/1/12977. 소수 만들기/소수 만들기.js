5eff159fdbc758a55c718f37f2cd7e590f371c1a
rhwjdgns <167050760+rhwjdgns@users.noreply.github.com>
2024-06-19 09:57:20 +0900

    [level 1] Title: 소수 만들기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,27 @@
+function solution(nums) {
+    let count = 0;
+    
+    function isPrime(n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 === 0 || n % 3 === 0) return false;
+        for (let i = 5; i * i <= n; i += 6) {
+            if (n % i === 0 || n % (i + 2) === 0) return false;
+        }
+        return true;
+    }
+    
+    const len = nums.length;
+    for (let i = 0; i < len - 2; i++) {
+        for (let j = i + 1; j < len - 1; j++) {
+            for (let k = j + 1; k < len; k++) {
+                const sum = nums[i] + nums[j] + nums[k];
+                if (isPrime(sum)) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
+}
