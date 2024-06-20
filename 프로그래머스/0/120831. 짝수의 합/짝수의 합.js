0def500378e64904b79d83ef0ec4f4f15b32373f
rhwjdgns <167050760+rhwjdgns@users.noreply.github.com>
2024-06-20 17:20:11 +0900

    [level 0] Title: 짝수의 합, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,7 +1,12 @@
 function solution(n) {
-    var answer = 0;
-    for (i = 0; i < (n+1); i += 2) {
-        answer += (i)
+  let sum = 0;
+  for (i = 0; i <= n; i++) {
+    if (i % 2 === 0) {
+      sum += i;
+      i++;
+    } else {
+      i++;
     }
-    return answer;
+  }
+  return sum;
 }
