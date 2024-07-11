96227060c84397f0ac10e3ede549be4be581ac1b
rhwjdgns <167050760+rhwjdgns@users.noreply.github.com>
2024-07-11 11:26:44 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.48 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,18 @@
+function solution(wallpaper) {
+  let lux = Infinity,
+    luy = Infinity;
+  let rdx = -Infinity,
+    rdy = -Infinity;
+
+  for (let i = 0; i < wallpaper.length; i++) {
+    for (let j = 0; j < wallpaper[i].length; j++) {
+      if (wallpaper[i][j] === "#") {
+        lux = Math.min(lux, i);
+        luy = Math.min(luy, j);
+        rdx = Math.max(rdx, i + 1);
+        rdy = Math.max(rdy, j + 1);
+      }
+    }
+  }
+  return [lux, luy, rdx, rdy];
+}
